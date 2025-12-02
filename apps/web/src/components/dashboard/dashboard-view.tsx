caf1d3982f93aec2f1988369ed70e0994326ba8d
TasseDeCafe <44320355+TasseDeCafe@users.noreply.github.com>
2025-12-02 01:32:37 +0100

    fix: add sign out button

--- apps/web/src/components/dashboard/dashboard-view.tsx
+++ apps/web/src/components/dashboard/dashboard-view.tsx
@@ -6,6 +6,7 @@ import { toast } from 'sonner'
 import { clearSentryUser } from '@/analytics/sentry/sentry-initializer'
 import { queryClient } from '@/config/react-query-config'
 import { NavbarContactButton } from '@/components/navbar/navbar-contact-button'
+import { Button } from '@/components/shadcn/button'
 import { getSupabaseClient } from '@/transport/third-party/supabase/supabase-client'
 
 export const DashboardView = () => {
@@ -24,13 +25,10 @@ export const DashboardView = () => {
     <div className='w-full flex-col items-center p-2 py-4 text-center'>
       {t`Dashboard.`}
       <div className='mt-4 flex justify-center gap-4'>
-        <button
-          onClick={handleSignOut}
-          className='flex cursor-pointer items-center rounded bg-gray-100 p-2 hover:bg-gray-300'
-        >
-          <LogOut size={20} className='mr-2' />
-          <span className='text-gray-800'>{t`Sign out`}</span>
-        </button>
+        <Button variant='ghost' onClick={handleSignOut}>
+          <LogOut size={20} />
+          <span>{t`Sign out`}</span>
+        </Button>
         <NavbarContactButton />
       </div>
     </div>
